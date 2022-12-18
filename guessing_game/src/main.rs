d7fdd0795998524dc6acc13cc45a26828606609a
Laurence Qi <laurence.qi@outlook.com>
2022-12-18 16:30:04 +1100

    Added read_line logic

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your number");
+
+    let mut guess = String::new(); // new is an associated function on String
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+    println!("You guessed: {guess}");
 }
